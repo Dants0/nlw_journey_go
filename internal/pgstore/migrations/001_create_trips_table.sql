cfdb50ff935159ddff5e72bf143846d464479f50
Guilherme Dantas Goes <guilhermedg@labcmi.org.br>
2024-07-09 12:05:20 -0300

    feat: first commit

---
+++ internal/pgstore/migrations/001_create_trips_table.sql
@@ -0,0 +1,15 @@
+-- Write your migrate up statements here
+
+create table if not exists trips (
+    "id" UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+    "destination" VARCHAR(255) NOT NULL,
+    "owner_email" VARCHAR(255) NOT NULL,
+    "owner_name" VARCHAR(255) NOT NULL,
+    "is_confirmed" BOOLEAN NOT NULL DEFAULT FALSE,
+    "starts_at" TIMESTAMP NOT NULL,
+    "ends_at" TIMESTAMP NOT NULL
+)
+
+---- create above / drop below ----
+
+drop table if exists trips;
---
+++ internal/pgstore/migrations/003_create_activities_table.sql
@@ -0,0 +1,16 @@
+-- Write your migrate up statements here
+
+CREATE TABLE IF NOT EXISTS activities(
+  "id" UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+  "trip_id" UUID NOT NULL,
+  "title" VARCHAR(255) NOT NULL,
+  "occurs_at" TIMESTAMP NOT NULL,
+  FOREIGN KEY (trip_id) REFERENCES trips(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+)
+
+---- create above / drop below ----
+
+-- Write your migrate down statements here. If this migration is irreversible
+-- Then delete the separator line above.
