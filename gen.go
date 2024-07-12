3915ebaf2a008f17f4e3aa4b25a8ec7f066ccb8b
Dants0 <gui.dg1@hotmail.com>
2024-07-12 00:16:26 -0300

    avançando

--- gen.go
+++ gen.go
@@ -1,4 +1,4 @@
 package main
 
 //go:generate tern migrate --migrations ./internal/pgstore/migrations --config ./internal/pgstore/migrations/tern.conf
-
+//sqlc generate -f ./internal/pgstore/sqlc.yaml
